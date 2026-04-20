a921d62c90ab9f22ccddae3520e4016367de2c6d
Coruni <mineimc@outlook.com>
2026-04-21 03:17:48 +0800

    🐞 fix: 删除内置字体

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,40 +1,6 @@
 import { AnalyticsScripts } from "@/components/providers/AnalyticsScripts";
-import localFont from "next/font/local";
 import { Suspense } from "react";
 import "./globals.css";
-
-const miSans = localFont({
-  src: [
-    {
-      path: "./fonts/MiSans-Normal.woff2",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "./fonts/MiSans-Regular.woff2",
-      weight: "500",
-      style: "normal",
-    },
-    {
-      path: "./fonts/MiSans-Semibold.woff2",
-      weight: "600",
-      style: "normal",
-    },
-  ],
-  fallback: [
-    "PingFang SC",
-    "Hiragino Sans GB",
-    "Microsoft YaHei",
-    "HarmonyOS Sans SC",
-    "Noto Sans SC",
-    "Segoe UI",
-    "Helvetica Neue",
-    "Arial",
-  ],
-  variable: "--font-misans",
-  display: "swap",
-  preload: false,
-});
 
 export default function RootLayout({
   children,
@@ -42,7 +8,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html suppressHydrationWarning className={miSans.className}>
+    <html suppressHydrationWarning>
       <body className="antialiased">
         <Suspense>
           <AnalyticsScripts />
